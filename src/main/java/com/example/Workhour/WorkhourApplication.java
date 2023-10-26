9599826a963204b5a7afb8de23ba985540b214bd
HarryNguye <harrynguyen100603@gmail.com>
2023-10-26 09:26:30 +0300

    First commit

---
+++ src/main/java/com/example/Workhour/WorkhourApplication.java
@@ -0,0 +1,13 @@
+package com.example.Workhour;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkhourApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkhourApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Workhour/WorkhourApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Workhour;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkhourApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
